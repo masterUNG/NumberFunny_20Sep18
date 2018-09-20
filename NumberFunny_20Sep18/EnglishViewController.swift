649100c0ebb5ae37fbbbffb0ddbd50cf83a2427e
masterUNG <phrombutr@gmail.com>
2018-09-20 13:55:56 +0700

    Create More MainStroy

---
+++ NumberFunny_20Sep18/EnglishViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EnglishViewController.swift
+//  NumberFunny_20Sep18
+//
+//  Created by MasterUNG on 20/9/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class EnglishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
